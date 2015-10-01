dd4c7b3fc6d9fa127c86f2d7c1533f5748bc72c6
Nirbheek Chauhan <nirbheek@centricular.com>
2015-10-01 18:08:46 +0530

    Add a wrapper for running one-video-app under valgrind

---
+++ memcheck-one-video-app.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# vim: set tw=0 :
+
+build_dir=$(dirname $0)
+
+: ${VALGRIND_LOG:="valgrind-memcheck.log"}
+GST_PLUGIN_PATH="${GST_PLUGIN_PATH}:${build_dir}/gst/proxy/.libs" libtool --mode=execute \
+	valgrind --suppressions="${build_dir}"/tests/supp/gst.supp --tool=memcheck --leak-check=full --leak-resolution=high --num-callers=12 --log-file="${VALGRIND_LOG}" \
+	"${build_dir}"/app/one-video-app "$@"
