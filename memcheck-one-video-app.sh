f13900d00f6b1c300a051ecbf77613056ec7e055
Nirbheek Chauhan <nirbheek@centricular.com>
2015-10-05 02:37:16 +0530

    memcheck-one-video-app.sh: Run the memory/slice allocators in a valgrind-friendly mode

--- memcheck-one-video-app.sh
+++ memcheck-one-video-app.sh
@@ -4,6 +4,7 @@
 build_dir=$(dirname $0)
 
 : ${VALGRIND_LOG:="valgrind-memcheck.log"}
-GST_PLUGIN_PATH="${GST_PLUGIN_PATH}:${build_dir}/gst/proxy/.libs" libtool --mode=execute \
+GST_PLUGIN_PATH="${GST_PLUGIN_PATH}:${build_dir}/gst/proxy/.libs" G_SLICE="always-malloc" G_DEBUG="$G_DEBUG,gc-friendly" \
+	libtool --mode=execute \
 	valgrind --suppressions="${build_dir}"/tests/supp/gst.supp --tool=memcheck --leak-check=full --leak-resolution=high --num-callers=12 --log-file="${VALGRIND_LOG}" \
 	"${build_dir}"/app/one-video-app "$@"
