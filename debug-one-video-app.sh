9542098411834d10bc52fff906ce9fc6b954d05a
Nirbheek Chauhan <nirbheek@centricular.com>
2015-10-05 02:37:37 +0530

    debug-one-video-app.sh: Abort on criticals for debugging

--- debug-one-video-app.sh
+++ debug-one-video-app.sh
@@ -3,4 +3,7 @@
 
 build_dir=$(dirname $0)
 
-GST_PLUGIN_PATH="${GST_PLUGIN_PATH}:${build_dir}/gst/proxy/.libs" libtool --mode=execute gdb --args "${build_dir}"/app/one-video-app "$@"
+GST_PLUGIN_PATH="${GST_PLUGIN_PATH}:${build_dir}/gst/proxy/.libs" G_DEBUG="fatal-criticals" \
+	libtool --mode=execute \
+	gdb --args \
+	"${build_dir}"/app/one-video-app "$@"
