5cc9f96164d9aa679100dfe29798211b7bc66b2a
Nirbheek Chauhan <nirbheek@centricular.com>
2015-10-05 07:56:48 +0530

    debug-one-video-app.sh: Don't overwrite pre-existing G_DEBUG values
    Also set a default value for GST_DEBUG

--- debug-one-video-app.sh
+++ debug-one-video-app.sh
@@ -3,7 +3,7 @@
 
 build_dir=$(dirname $0)
 
-GST_PLUGIN_PATH="${GST_PLUGIN_PATH}:${build_dir}/gst/proxy/.libs" G_DEBUG="fatal-criticals" \
+GST_PLUGIN_PATH="${GST_PLUGIN_PATH}:${build_dir}/gst/proxy/.libs" G_DEBUG="$G_DEBUG,fatal-criticals" GST_DEBUG="*:3,onevideo:6,$GST_DEBUG" \
 	libtool --mode=execute \
 	gdb --args \
 	"${build_dir}"/app/one-video-app "$@"
