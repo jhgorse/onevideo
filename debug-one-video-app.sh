7e26348cbaed3e7ca25cb7ef03be53d17343cffa
Nirbheek Chauhan <nirbheek@centricular.com>
2015-11-10 03:48:51 +0530

    debug-one-video-app.sh: Also abort on glib warnings

--- debug-one-video-app.sh
+++ debug-one-video-app.sh
@@ -5,7 +5,7 @@ build_dir=$(dirname $0)
 
 . "${build_dir}/export-path.sh"
 
-G_DEBUG="$G_DEBUG,fatal-criticals" GST_DEBUG="*:3,onevideo:6,$GST_DEBUG" \
+G_DEBUG="$G_DEBUG,fatal-warnings" GST_DEBUG="*:3,onevideo:6,$GST_DEBUG" \
 	libtool --mode=execute \
 	gdb --args \
 	"${build_dir}"/app/one-video-app "$@"
