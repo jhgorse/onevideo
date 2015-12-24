2f213df62251e2642a273941f503d9a104c34ba1
Nirbheek Chauhan <nirbheek@centricular.com>
2015-12-24 09:27:18 +0530

    debug wrapper: Don't abort on critical/warnings
    I added it just for testing, but now it impedes testing

--- wrappers/debug-one-video-wrapper.sh
+++ wrappers/debug-one-video-wrapper.sh
@@ -6,7 +6,8 @@ cur_dir="${build_dir}/wrappers"
 
 . "${cur_dir}/setup.sh"
 
-G_DEBUG="$G_DEBUG,fatal-warnings" GST_DEBUG="*:3,onevideo:6,$GST_DEBUG" \
+#G_DEBUG="$G_DEBUG,fatal-warnings"
+GST_DEBUG="*:3,onevideo:6,$GST_DEBUG" \
 	libtool --mode=execute \
 	gdb --args \
 	"${build_dir}"/${progtype}/one-video-${progtype} "$@"
