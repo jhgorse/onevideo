b188a7bcd1479514f78226c5cf202cffd35f9723
Nirbheek Chauhan <nirbheek@centricular.com>
2016-02-11 00:33:48 +0530

    wrappers/memcheck: Only show definitely and indirectly lost
    Possibly lost adds too much noise and is usually caused due to GLib's GSlice
    allocator

--- wrappers/memcheck-one-video-wrapper.sh
+++ wrappers/memcheck-one-video-wrapper.sh
@@ -9,5 +9,5 @@ cur_dir="${build_dir}/wrappers"
 : ${VALGRIND_LOG:="valgrind-memcheck.log"}
 G_SLICE="always-malloc" G_DEBUG="$G_DEBUG,gc-friendly" GST_DEBUG="*:3,onevideo:6,$GST_DEBUG" \
 	$LIBTOOL --mode=execute \
-	valgrind --suppressions="${build_dir}"/tests/supp/gst.supp --tool=memcheck --leak-check=full --leak-resolution=high --num-callers=30 --log-file="${VALGRIND_LOG}" \
+	valgrind --suppressions="${build_dir}"/tests/supp/gst.supp --tool=memcheck --leak-check=full --show-leak-kinds=definite,indirect --leak-resolution=high --num-callers=30 --log-file="${VALGRIND_LOG}" \
 	"${build_dir}"/${progtype}/one-video-${progtype} "$@"
